423337b98cca3a9b2dc4459df6af69c0b36a3d49
MusthafaLuthfi76 <musthafa.luthfi76@gmail.com>
2023-06-23 10:57:03 +0700

    Cek apakah antrian kosong

--- Cicrcular Queues/Cicrcular Queues.cpp
+++ Cicrcular Queues/Cicrcular Queues.cpp
@@ -55,8 +55,23 @@ public :
             if (FRONT == max - 1)
                 FRONT = 0;
             else
-                FRONT = FRONT +1
+                FRONT = FRONT + 1;
         }
+    }
+
+    void display() {
+        int FRONT_position = FRONT;
+        int REAR_position = REAR;
+
+        //Cek apakah antrian kosong
+        if (FRONT == -1) {
+            cout << "Queue is empty\n";
+            return;
+        }
+
+        cout << "\nElemen in the queu are ...\n";
+
+        //Jika FRONT_position <= REAR_position
     }
 };
 
