b0c1a1d62960dec9e041c4fa491decad9510d85a
MusthafaLuthfi76 <musthafa.luthfi76@gmail.com>
2023-06-23 11:19:43 +0700

    Setting default

--- Cicrcular Queues/Cicrcular Queues.cpp
+++ Cicrcular Queues/Cicrcular Queues.cpp
@@ -131,6 +131,10 @@ int main()
             case '4': {
                 return 0;
             }
+            default: {
+                cout << "Invalid Option!!" << endl;
+                break;
+            }
             }
         }
     }
